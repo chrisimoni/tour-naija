c56ba2735368e3afda9d1b31deeb8aaab24285bd
Imoni Christopher <imonireal@gmail.com>
2020-02-09 18:13:35 +0100

    build a more complex email handler

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,53 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  //Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Chris Imoni <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid
+      return 1;
     }
-  });
 
-  //Define email options
-  const mailOptions = {
-    from: 'Chris Imoni <cimoni@ncgafrica.com>', // sender address
-    to: options.email, // list of receivers
-    subject: options.subject, // Subject line
-    text: options.message // plain text body
-    //html: '<b>Hello world?</b>' // html body
-  };
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  //Send the mail
-  await transporter.sendMail(mailOptions);
+  async send(template, subject) {
+    //Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
+
+    //Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    //Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('Welcome', 'Welcome to the best Naija tour experience');
+  }
 };
-
-module.exports = sendEmail;
