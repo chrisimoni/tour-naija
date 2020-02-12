ef5f08bc15344e145383c91ad463ae55d85635ae
Imoni Christopher <imonireal@gmail.com>
2020-02-12 09:07:47 +0100

    create new bookings on checkout success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,17 +1,19 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const Tour = require('./../models/tourModel');
-const catchAsyn = require('./../utils/catchAsync');
-// const AppError = require('./../utils/appError');
-// const factory = require('./handleFactory');
+const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
+//const factory = require('./handlerFactory');
 
-exports.getCheckoutSession = catchAsyn(async (req, res, next) => {
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
 
   //Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -32,4 +34,14 @@ exports.getCheckoutSession = catchAsyn(async (req, res, next) => {
     status: 'success',
     session
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9067,7 +9067,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61613" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50722" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
